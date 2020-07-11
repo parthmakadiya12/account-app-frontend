e171696d9465f0d4e576268758db086540fa1383
Parth Makadiya <parth.makadiya@hexad.de>
2020-07-11 13:38:42 +0200

    design login page

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -1,4 +1,14 @@
 import React from "react";
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import Link from "@material-ui/core/Link";
+import Grid from "@material-ui/core/Grid";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import Typography from "@material-ui/core/Typography";
+import Container from "@material-ui/core/Container";
+import { withStyles } from "@material-ui/core/styles";
 
 class Login extends React.Component {
   constructor(props) {
@@ -8,6 +18,7 @@ class Login extends React.Component {
       password: "",
     };
   }
+
 
   handleInputChange = (e, name) => {
     this.setState({
@@ -23,34 +34,84 @@ class Login extends React.Component {
   };
 
   render() {
-    const { username, password } = this.state;
-    const { spinner } = this.props;
-
-    if (spinner === true) {
-      return <div>Loading</div>;
-    }
+    const { classes } = this.props;
 
     return (
-      <div>
-        Login Page Username:
-        <input
-          type="text"
-          onChange={(e) => this.handleInputChange(e, "username")}
-          value={username}
-        />
-        Password:
-        <input
-          type="password"
-          onChange={(e) => this.handleInputChange(e, "password")}
-          value={password}
-        />
-        <button onClick={this.performLogin}>Perform Login</button>
-        <button onClick={() => this.props.history.push("/signup")}>
-          Signup
-        </button>
-      </div>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <div className={classes.paper}>
+          <Avatar className={classes.avatar}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+          <form className={classes.form} noValidate>
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              onChange={(e) => this.handleInputChange(e, "username")}
+              id="username"
+              label="Username"
+              name="username"
+              autoComplete="username"
+              autoFocus
+            />
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              onChange={(e) => this.handleInputChange(e, "password")}
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+            <Button
+              fullWidth
+              variant="contained"
+              color="primary"
+              onClick={this.performLogin}
+              className={classes.submit}
+            >
+              Sign In
+            </Button>
+            <Grid container>
+
+              <Grid item>
+                <Link href="/signup" variant="body2">
+                  {"Don't have an account? Sign Up"}
+                </Link>
+              </Grid>
+            </Grid>
+          </form>
+        </div>
+      </Container>
     );
   }
 }
+const styles = theme => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+});
 
-export default Login;
+export default withStyles(styles, { withTheme: true })(Login);
